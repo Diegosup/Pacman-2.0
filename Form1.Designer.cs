0f30da340ec33e0a9610587de9f6903cd34620ae
Diegosup <110991667+Diegosup@users.noreply.github.com>
2023-02-07 21:01:38 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Pacman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.level1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.level2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.level3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LBL_STATUS = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PCT_CANVAS
+            // 
+            this.PCT_CANVAS.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PCT_CANVAS.Location = new System.Drawing.Point(0, 33);
+            this.PCT_CANVAS.Name = "PCT_CANVAS";
+            this.PCT_CANVAS.Size = new System.Drawing.Size(1134, 612);
+            this.PCT_CANVAS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PCT_CANVAS.TabIndex = 0;
+            this.PCT_CANVAS.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 120;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1134, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.level1ToolStripMenuItem,
+            this.level2ToolStripMenuItem,
+            this.level3ToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(118, 29);
+            this.toolStripMenuItem1.Text = "Select Level";
+            // 
+            // level1ToolStripMenuItem
+            // 
+            this.level1ToolStripMenuItem.Name = "level1ToolStripMenuItem";
+            this.level1ToolStripMenuItem.Size = new System.Drawing.Size(163, 34);
+            this.level1ToolStripMenuItem.Text = "Level1";
+            this.level1ToolStripMenuItem.Click += new System.EventHandler(this.Level1_Click);
+            // 
+            // level2ToolStripMenuItem
+            // 
+            this.level2ToolStripMenuItem.Name = "level2ToolStripMenuItem";
+            this.level2ToolStripMenuItem.Size = new System.Drawing.Size(163, 34);
+            this.level2ToolStripMenuItem.Text = "Level2";
+            this.level2ToolStripMenuItem.Click += new System.EventHandler(this.Level2_Click);
+            // 
+            // level3ToolStripMenuItem
+            // 
+            this.level3ToolStripMenuItem.Name = "level3ToolStripMenuItem";
+            this.level3ToolStripMenuItem.Size = new System.Drawing.Size(163, 34);
+            this.level3ToolStripMenuItem.Text = "Level3";
+            this.level3ToolStripMenuItem.Click += new System.EventHandler(this.Level3_Click);
+            // 
+            // LBL_STATUS
+            // 
+            this.LBL_STATUS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LBL_STATUS.AutoSize = true;
+            this.LBL_STATUS.Location = new System.Drawing.Point(930, 11);
+            this.LBL_STATUS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBL_STATUS.Name = "LBL_STATUS";
+            this.LBL_STATUS.Size = new System.Drawing.Size(66, 20);
+            this.LBL_STATUS.TabIndex = 2;
+            this.LBL_STATUS.Text = "SCORE";
+            // 
+            // Score
+            // 
+            this.Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Score.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Score.Enabled = false;
+            this.Score.Location = new System.Drawing.Point(1005, 6);
+            this.Score.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Score.Name = "Score";
+            this.Score.ReadOnly = true;
+            this.Score.Size = new System.Drawing.Size(109, 26);
+            this.Score.TabIndex = 3;
+            this.Score.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 645);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.LBL_STATUS);
+            this.Controls.Add(this.PCT_CANVAS);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PCT_CANVAS;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem level1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem level2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem level3ToolStripMenuItem;
+        private System.Windows.Forms.Label LBL_STATUS;
+        private System.Windows.Forms.TextBox Score;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        byte[,] level = Class1.map0;
+        byte[,] NewLevel1 = Class1.N0;//Guardar mapas para que no se borren 
+        //byte[,] NewLevel2 = Class1.N1;
+        //byte[,] NewLevel3 = Class1.N2;
+        int pills = 0;
+        int xpos, ypos, count;
+        bool goright, goleft, goup, godown;
+        bool firstTime;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(250, 250);
+            PCT_CANVAS.Image = bmp;
+            firstTime = true;
+            DrawMap(level);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (goleft == true)
+            {
+                Move(xpos, ypos, xpos - 1, ypos);
+            }
+            if (goright == true)
+            {
+                Move(xpos, ypos, xpos + 1, ypos);
+            }
+            if (goup == true)
+            {
+                Move(xpos, ypos, xpos, ypos - 1);
+            }
+            if (godown == true)
+            {
+                Move(xpos, ypos, xpos, ypos + 1);
+            }
+        }
+
+        //Resetear mapas
+        private void restart(byte[,] currentMap, byte[,] newMap)
+        {
+            for (int x = 0; x < currentMap.GetLength(1); x++)
+            {
+                for (int y = 0; y < currentMap.GetLength(1); y++)
+                {
+                    currentMap[y, x] = newMap[y, x];
+                }
+            }
+            //Resetear SCORE y dibujar de nuevo el mapa
+            count = 0;
+            Score.Text = count.ToString();
+            PCT_CANVAS.Image = bmp;
+            firstTime = true;
+            DrawMap(currentMap);
+
+        }
+        private void Level1_Click(object sender, EventArgs e)
+        {
+            pills = 0;
+            restart(level, Class1.N0);
+
+        }
+
+        private void Level2_Click(object sender, EventArgs e)
+        {
+            pills = 0;
+            restart(level, Class1.N1);
+
+        }
+
+        private void Level3_Click(object sender, EventArgs e)
+        {
+            pills = 0;
+            restart(level, Class1.N2);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    goleft = true;
+                    goright = false;
+                    godown = false;
+                    goup = false;
+                    break;
+                case Keys.Right:
+                    goright = true;
+                    goleft = false;
+                    godown = false;
+                    goup = false;
+
+                    break;
+                case Keys.Up:
+                    goup = true;
+                    goleft = false;
+                    godown = false;
+                    goright = false;
+                    break;
+                case Keys.Down:
+                    godown = true;
+                    goleft = false;
+                    goright = false;
+                    goup = false;
+                    break;
+                case Keys.Space:
+                    break;
+            }
+        }
+
+        private void DrawMap(byte[,] map)
+        {
+
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.Black);
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+
+                    if (map[y, x] == 3)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                        g.DrawImage(Resource1.PG, x * 10, y * 10, 10, 10);
+                    }
+                    if (map[y, x] == 4)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                        g.DrawImage(Resource1.RG, x * 10, y * 10, 10, 10);
+                    }
+                    if (map[y, x] == 5)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                        g.DrawImage(Resource1.YG, x * 10, y * 10, 10, 10);
+                    }
+                    if (map[y, x] == 6)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                        g.DrawImage(Resource1.BG, x * 10, y * 10, 10, 10);
+                    }
+                    if (map[y, x] == 0)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                    }
+                    if (map[y, x] == 8)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                        g.DrawImage(Resource1.YDOT, x * 10, y * 10, 10, 10);
+
+                    }
+                    if (map[y, x] == 2)
+                    {
+                        if (firstTime == true)
+                        {
+                            g.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 128)), x * 10, y * 10, 10, 10);
+                            g.DrawImage(Resource2.pacmanright, x * 10, y * 10, 10, 10);
+                            firstTime = false;
+                        }
+
+
+                        if (goright == true)
+                            g.DrawImage(Resource2.pacmanright, x * 10, y * 10, 10, 10);
+
+                        if (goleft == true)
+                            g.DrawImage(Resource2.pacmanleft, x * 10, y * 10, 10, 10);
+
+                        if (godown == true)
+                            g.DrawImage(Resource2.pacmandown, x * 10, y * 10, 10, 10);
+
+                        if (goup == true)
+                            g.DrawImage(Resource2.pacmanup, x * 10, y * 10, 10, 10);
+
+                        xpos = x;
+                        ypos = y;
+                    }
+                    else
+                        g.DrawRectangle(Pens.Gray, x * 10, y * 10, 10, 10);
+                }
+            }
+            PCT_CANVAS.Invalidate();
+
+        }
+
+        private void Move(int oldx, int oldy, int newx, int newy)
+        {
+            int limy, limx, nextPos;
+            limy = level.GetLength(1) - 1;
+            limx = level.GetLength(0) - 1;
+            if (newy > limy)
+            {
+                level[0, newx] = 2;
+                level[oldy, oldx] = 0;
+            }
+            else
+            {
+                if (newy < 0)
+                {
+                    level[limy, newx] = 2;
+                    level[oldy, oldx] = 0;
+                }
+                else
+                {
+                    if (newx > limx)
+                    {
+                        level[newy, 0] = 2;
+                        level[oldy, oldx] = 0;
+                    }
+                    else
+                    {
+                        if (newx < 0)
+                        {
+                            level[newy, limx] = 2;
+                            level[oldy, oldx] = 0;
+                        }
+                        else
+                        {
+                            if (level[newy, newx] != 1)
+                            {
+                                //Contar Monedas
+                                if (level[newy, newx] == 8)
+                                {
+                                    count = int.Parse(Score.Text);
+                                    count++;
+                                    Score.Text = count.ToString();
+                                    //llega a comer 10 pildoras
+                                    if (count == 10)
+                                    {
+                                        timer1.Enabled = false;
+                                        MessageBox.Show("YOU WON");
+                                    }
+                                }
+                                //Detectar colision con fantasma
+                                if (level[newy, newx] >= 3 && level[newy, newx] <= 6)
+                                {
+                                    timer1.Enabled = false;
+                                    DialogResult dr = MessageBox.Show(" CONTINUE?", "GAME OVER", MessageBoxButtons.YesNo);
+                                    switch (dr)
+                                    {
+                                        case DialogResult.Yes:
+                                            Application.Restart();
+                                            break;
+                                        case DialogResult.No:
+                                            Application.Exit();
+                                            break;
+                                    }
+
+
+                                }
+                                level[oldy, oldx] = 0;
+                                level[newy, newx] = 2;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+
+            DrawMap(level);
+        }
+
+        public void UpdateMap()
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.Black);
+            for (int x = 0; x < level.GetLength(0); x++)
+            {
+                for (int y = 0; y < level.GetLength(1); y++)
+                {
+                    if (level[y, x] == 2)
+                    {
+                        g.DrawImage(Resource1.pacman, x * 10, y * 10, 10, 10);
+
+                    }
+                }
+            }
+        }
+    }
+}
